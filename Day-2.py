874fbca8ced83b910a05993a80f17f3d95947c3a
praveensinghkhangarot <praveensinghkhangarot3@gmail.com>
2024-05-14 14:12:28 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,50 @@
+
+# print("hello")
+# var1=10
+# var2=25,21
+# var3=True 
+# var4="Upflair"
+# var5="jecrc"
+# var6="""jecrc foundation"""
+# print(var1)
+# print(var2)
+# print(var3)
+# print(var4)
+# print(var5)
+# print(var6)
+
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+# print(type(var5))
+# print(type(var6))
+
+# rupees=25000
+# print("Ihave =",rupees,"rupees")
+# paragraph="""gggggggggggggggggggggggggggggggggggggggggggg
+# gggggggggggggggggggggggggggggggggggggggggggg
+# ggggggggggggggggggggggggggggggggggg
+# """
+# print(paragraph)
+# college="JECRC FOUNDATION"
+# print(college.upper())
+# print(college.lower())
+# print(len(college))
+# print(college.count('c'))
+# print(college.endswith('N'))
+# print(college.endswith('P'))
+# college="jecrc"
+# print(college.strip())
+# print(college.lstrip())
+# print(college.rstrip())
+# print(college[3])
+#print(college[3:5:-1])
+# college="jecrc"
+
+#same hai dono
+# print(college[::-1])
+# print(college[4::-1])
+college="jecrc jaipur rajasthan"
+print(college[-1])
+print(college[0::])
