db8e726a28226d82d5372b89681a589ed0e32292
praveensinghkhangarot <praveensinghkhangarot3@gmail.com>
2024-05-14 15:03:16 +0530

    sender file upload

---
+++ Day-3.py
@@ -0,0 +1,54 @@
+
+# lst=[23,45,'praveen',True,35.45]
+# print(lst[1:4])
+# lst[1]=500
+# print(lst)
+# print(len(lst))
+# print(lst)
+# lst.insert(1,'jecrc')
+# lst.append(100)
+# lst.append(200)
+# lst.pop()
+# print(lst)
+# print([3])
+# print(lst[2][-1])
+# lst=[100,23,45,35,32,23,[43,42,41,20]]
+# print(lst[6][2])
+# lst[6][2]=10000
+# print(lst)
+# tpl=(10,20,30,40.9,'praveen',True)
+# print(tpl)
+# print(type(tpl))
+# print(tpl[1])
+# tpl[1]=300
+# print(tpl)
+# college_name = ('jecrc',)
+# print(college_name)
+# print(type(college_name))
+# college_name = "jecrc",
+# print(college_name)
+# print(type(college_name))
+# lst=[10,20,30,40]
+# print(lst*2)
+# print(lst)
+# lst.remove(20)
+# print(lst)
+# name="Praveen "
+# surname="Singh"
+# roll_no=121
+# print(name+roll_no)
+# tpl=(10,20,30)
+# print(tpl*4
+# lst1=[1,2,3,4,5]
+# lst2=[6,7,8,9,10]
+# lst1.extend(lst2)
+# print(lst1)
+# pair ke form me store hoti hai (keys,values)
+# dt={"name":"praveen","branch":"IT"}
+# print(dt["name"])
+# print(type(dt))
+# print(dt.keys())
+# print(dt.values())
+dt={"name":"praveen","branch":"IT","roll_no":"121"}
+dt={0:"praveen",1:"IT",2:"121"}
+class_data={"name":["a","b","c","d"]}
